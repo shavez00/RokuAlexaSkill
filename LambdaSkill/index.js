48ff69f3e06e2e3e184e18576f8ed2898eac4a28
shavez00 <shavez00@yahoo.com>
2017-01-27 14:44:30 -0800

    Updated with Search functionality and dynamic Channel selection

--- LambdaSkill/index.js
+++ LambdaSkill/index.js
@@ -3,9 +3,32 @@ var APP_ID = null; //replace this with your app ID to make use of APP_ID verific
 var AlexaSkill = require("./AlexaSkill");
 var serverinfo = require("./serverinfo");
 var http = require("http");
+const querystring = require('querystring');
 
 if (serverinfo.host == "127.0.0.1") {
     throw "Default hostname found, edit your serverinfo.js file to include your server's external IP address";
+}
+
+function getChannels(intent,session,response) {
+    var options = {
+        host: 'vezcore-qvtwdtbnjp.dynamic-m.com',
+        port: 1234,
+        path: '/roku/getListofChannels',
+        headers: {'Authorization': serverinfo.pass}
+    };
+
+    http.get(options, function(res) {
+            res.on('data', function (chunk) {
+                var jsonContent = JSON.parse(chunk);
+                var chan = intent.slots.Channel.value;
+                var id = chan.toLowerCase();
+                console.log('BODY: ' + jsonContent[id]);
+                sendCommand("/roku/launch/"+jsonContent[id],null,function(){
+                    response.tellWithCard("Launching "+intent.slots.Channel.value);
+                });
+                //return jsonContent;
+            });
+    });
 }
 
 var AlexaRoku = function () {
@@ -36,25 +59,32 @@ function sendCommand(path,body,callback) {
     req.end();
 }
 
+//need to adjust responses to respond with name of roku device being used, ie "Going Home on Upstairs Roku"
 AlexaRoku.prototype.intentHandlers = {
     Home: function (intent, session, response) {
         sendCommand("/roku/keypress/home",null,function() {
             response.tellWithCard("Going Home");
         });
     },
-    History: function (intent, session, response) {
-        sendCommand("/roku/launch/12",null,function() {
-            response.tellWithCard("Launching Netflix");
+    PlayChannel: function (intent, session, response) {
+        getChannels(intent, session, response);
+    },
+    RokuSearch: function (intent, session, response) {
+        var query = intent.slots.Search.value;
+        var searchString = querystring.stringify({title : query});
+        console.log("/roku/search/browse?"+searchString);
+        sendCommand("/roku/search/browse?"+searchString,null,function() {
+            response.tellWithCard("Searching for "+intent.slots.Search.value);
         });
     },
-    Netflix: function (intent, session, response) {
+    BloombergTV: function (intent, session, response) {
+        sendCommand("/roku/launch/54000",null,function() {
+            response.tellWithCard("Launching Bloomberg News");
+        });
+    },
+    History: function (intent, session, response) {
         sendCommand("/roku/launch/35059",null,function() {
             response.tellWithCard("Launching The History Channel");
-        });
-    },
-    Vevo: function (intent, session, response) {
-        sendCommand("/roku/launch/20445",null,function() {
-            response.tellWithCard("Launching Vevo");
         });
     },
     Amazon: function (intent, session, response) {
@@ -62,9 +92,14 @@ AlexaRoku.prototype.intentHandlers = {
             response.tellWithCard("Launching Amazon");
         });
     },
-    Pandora: function (intent, session, response) {
-        sendCommand("/roku/launch/28",null,function() {
-            response.tellWithCard("Launching Pandora");
+    WatchESPN: function (intent, session, response) {
+        sendCommand("/roku/launch/34376",null,function() {
+            response.tellWithCard("OK, let's watch some sports");
+        });
+    },
+    CNNGo: function (intent, session, response) {
+        sendCommand("/roku/launch/65978",null,function() {
+            response.tellWithCard("Launching CNN News");
         });
     },
     Select: function (intent, session, response) {
@@ -94,9 +129,9 @@ AlexaRoku.prototype.intentHandlers = {
         });
     
     },
-    FourK: function (intent, session, response) {
-        sendCommand("/roku/launch/69091",null,function() {
-            response.tellWithCard("Launching Four K");
+    btngo: function (intent, session, response) {
+        sendCommand("/roku/launch/66258",null,function() {
+            response.tellWithCard("Launching The Big Ten Network");
         });
     
     },
@@ -119,40 +154,105 @@ AlexaRoku.prototype.intentHandlers = {
     
     },
     Up: function (intent, session, response) {
-        sendCommand("/roku/keypress/up",null,function() {
+        sendCommand("/roku/keypress/Up",null,function() {
             response.tellWithCard("Up");
         });
     },
+    UpTwo: function (intent, session, response) {
+        sendCommand("/roku/keypress/Up",null,function() {});
+        sendCommand("/roku/keypress/Up",null,function() {
+            response.tellWithCard("Up Two");
+        });
+    },
+    UpThree: function (intent, session, response) {
+        sendCommand("/roku/keypress/Up",null,function() {});
+        sendCommand("/roku/keypress/Up",null,function() {});
+        sendCommand("/roku/keypress/Up",null,function() {
+            response.tellWithCard("Up Three");
+        });
+    },
     Down: function (intent, session, response) {
-        sendCommand("/roku/keypress/down",null,function() {
+        sendCommand("/roku/keypress/Down",null,function() {
             response.tellWithCard("Down");
+        });
+    },
+    DownTwo: function (intent, session, response) {
+        sendCommand("/roku/keypress/Down",null,function() {});
+        sendCommand("/roku/keypress/Down",null,function() {
+            response.tellWithCard("Down Two");
+        });
+    },
+    DownThree: function (intent, session, response) {
+        sendCommand("/roku/keypress/Down",null,function() {});
+        sendCommand("/roku/keypress/Down",null,function() {});
+        sendCommand("/roku/keypress/Down",null,function() {
+            response.tellWithCard("Down Three");
         });
     },
     PowerOn: function (intent, session, response) {
         sendCommand("/roku/keypress/poweron",null,function() {
-            response.tellWithCard("Power On");
+            response.tellWithCard("OK, fasten your seat belts.  Here comes the fun");
         });
     },
     PowerOff: function (intent, session, response) {
         sendCommand("/roku/keypress/poweroff",null,function() {
-            response.tellWithCard("Power Off");
+            response.tellWithCard("OK, I turned off the Roku TV");
         });
     },
     Left: function (intent, session, response) {
-        sendCommand("/roku/keypress/left",null,function() {
+        sendCommand("/roku/keypress/Left",null,function() {
             response.tellWithCard("Left");
         });
-    
+    },
+    LeftTwo: function (intent, session, response) {
+        sendCommand("/roku/keypress/Left",null,function() {});
+        sendCommand("/roku/keypress/Left",null,function() {
+            response.tellWithCard("Left Two");
+        });
+    },
+    LeftThree: function (intent, session, response) {
+        sendCommand("/roku/keypress/Left",null,function() {});
+        sendCommand("/roku/keypress/Left",null,function() {});
+        sendCommand("/roku/keypress/Left",null,function() {
+            response.tellWithCard("Left Three");
+        });
     }, 
     Right: function (intent, session, response) {
         sendCommand("/roku/keypress/right",null,function() {
             response.tellWithCard("Right");
         });
-    
-    },   
+    },
+    RightTwo: function (intent, session, response) {
+        sendCommand("/roku/keypress/right",null,function() {});
+        sendCommand("/roku/keypress/right",null,function() {
+            response.tellWithCard("Right Two");
+        });
+    },
+    RightThree: function (intent, session, response) {
+        sendCommand("/roku/keypress/right",null,function() {});
+        sendCommand("/roku/keypress/right",null,function() {});
+        sendCommand("/roku/keypress/right",null,function() {
+            response.tellWithCard("Right Three");
+        });
+    },
     Type: function (intent, session, response) {
         sendCommand("/roku/type",intent.slots.Text.value,function() {
             response.tellWithCard("Typing text: "+intent.slots.Text.value,"Roku","Typing text: "+intent.slots.Text.value);
+        });
+    },
+    VolumeUp: function (intent, session, response) {
+        sendCommand("/roku/keypress/VolumeUp",null,function() {
+            response.tellWithCard("Ok, turning it up");
+        });
+    },
+    VolumeDown: function (intent, session, response) {
+        sendCommand("/roku/keypress/VolumeDown",null,function() {
+            response.tellWithCard("Ok, turning it down");
+        });
+    },
+    Mute: function (intent, session, response) {
+        sendCommand("/roku/keypress/VolumeMute",null,function() {
+            response.tellWithCard("Muted");
         });
     },
     PlayPause: function (intent, session, response) {
@@ -179,5 +279,3 @@ exports.handler = function (event, context) {
     var roku = new AlexaRoku();
     roku.execute(event, context);
 };
-
-
